be91b392d47475f7f8b8e96bbb6c6432d8d637a1
thaibm <thaibm.uet@gmail.com>
2017-02-03 14:50:51 +0700

    Repair indent

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,26 +5,26 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
-  	assert @user.valid?
+    assert @user.valid?
   end
 
   test "name should be present" do
-  	@user.name = ""
-  	assert_not @user.valid?
+    @user.name = ""
+    assert_not @user.valid?
   end
 
   test "email should be present" do
-  	@user.email = "     "
-  	assert_not @user.valid?
+    @user.email = "     "
+    assert_not @user.valid?
   end
 
   test "name should not be too long" do
-  	@user.name = "a" * 51
-  	assert_not @user.valid?
+    @user.name = "a" * 51
+    assert_not @user.valid?
   end
 
   test "email should not be too long" do
@@ -33,8 +33,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
