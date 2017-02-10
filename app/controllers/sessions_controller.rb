68ac55c3d3f37ab03e008e7a040b9672b355f284
thaibm <thaibm.uet@gmail.com>
2017-02-10 10:48:16 +0700

    Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = I18n.t".account_not_activated"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t".invalid"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = I18n.t ".welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = I18n.t :check_mail
+      redirect_to root_url
     else
       render :new
     end
